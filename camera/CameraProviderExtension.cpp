cf420234c9211fd753d6ddb161003c90b8b4f14b
kleidione Freitas <kleidione@gmail.com>
2026-03-10 08:33:36 -0300

    Spacewar: torch: Prevent led state corruption on toggle off
    Signed-off-by: kleidione <kleidione@gmail.com>

--- camera/CameraProviderExtension.cpp
+++ camera/CameraProviderExtension.cpp
@@ -5,11 +5,9 @@
  */
 
 #include "CameraProviderExtension.h"
-
 #include <fstream>
 
 #define TORCH_BRIGHTNESS "brightness"
-#define TORCH_MAX_BRIGHTNESS "max_brightness"
 #define TOGGLE_SWITCH "/sys/devices/platform/soc/c440000.qcom,spmi/spmi-0/spmi0-02/c440000.qcom,spmi:qcom,pm8350c@2:qcom,flash_led@ee00/leds/led:switch_2/brightness"
 
 static std::string kTorchLedPaths[] = {
@@ -17,24 +15,16 @@ static std::string kTorchLedPaths[] = {
         "/sys/devices/platform/soc/c440000.qcom,spmi/spmi-0/spmi0-02/c440000.qcom,spmi:qcom,pm8350c@2:qcom,flash_led@ee00/leds/led:torch_1",
 };
 
-/**
- * Write value to path and close file.
- */
 template <typename T>
 static void set(const std::string& path, const T& value) {
     std::ofstream file(path);
     file << value;
 }
 
-
-/**
- * Read value from the path and close file.
- */
 template <typename T>
 static T get(const std::string& path, const T& def) {
     std::ifstream file(path);
     T result;
-
     file >> result;
     return file.fail() ? def : result;
 }
@@ -47,33 +37,45 @@ bool supportsSetTorchModeExt() {
     return false;
 }
 
+static constexpr int32_t kTorchMinStrengthLevel     = 10;
+static constexpr int32_t kTorchDefaultStrengthLevel = 80;
+static constexpr int32_t kTorchMaxStrengthLevel     = 200;
+
 int32_t getTorchDefaultStrengthLevelExt() {
-    return 80;
+    return kTorchDefaultStrengthLevel;
 }
 
 int32_t getTorchMaxStrengthLevelExt() {
-    // 200 (out of 500) is a sane max brightness
-    return 200;
+    return kTorchMaxStrengthLevel;
 }
 
 int32_t getTorchStrengthLevelExt() {
-    // We write same value in the both LEDs,
-    // so get from one.
     auto node = kTorchLedPaths[0] + "/" + TORCH_BRIGHTNESS;
-    return get(node, 0);
+    int32_t val = get(node, kTorchMinStrengthLevel);
+    return (val < kTorchMinStrengthLevel) ? kTorchMinStrengthLevel : val;
 }
 
 void setTorchStrengthLevelExt(int32_t torchStrength, bool enabled) {
-    set(TOGGLE_SWITCH, 0);
-    for (auto& path : kTorchLedPaths) {
-        auto node = path + "/" + TORCH_BRIGHTNESS;
-        set(node, torchStrength);
-    }
-    if (enabled)
+    if (enabled) {
+        int32_t effectiveStrength = (torchStrength < kTorchMinStrengthLevel)
+                                        ? kTorchMinStrengthLevel
+                                        : torchStrength;
+
+        for (auto& path : kTorchLedPaths) {
+            set(path + "/" + TORCH_BRIGHTNESS, effectiveStrength);
+        }
         set(TOGGLE_SWITCH, 255);
+    } else {
+        set(TOGGLE_SWITCH, 0);
+    }
 }
 
 void setTorchModeExt(bool enabled) {
-    int32_t strength = getTorchDefaultStrengthLevelExt();
-    setTorchStrengthLevelExt(enabled ? strength : 0, enabled);
+    if (enabled) {
+        int32_t strength = getTorchStrengthLevelExt();
+        if (strength <= 0) strength = kTorchDefaultStrengthLevel;
+        setTorchStrengthLevelExt(strength, true);
+    } else {
+        setTorchStrengthLevelExt(0, false);
+    }
 }
